d4d112b28188d72017908cee7025abd2759a0bd3
Cloud Wu <cloudwu@gmail.com>
2016-01-07 12:54:38 +0800

    gc in worker thread

--- terminator.lua
+++ terminator.lua
@@ -1,19 +1,33 @@
-local fw = require "skynet.framework"
+local mainfunc
 
-if arg then
+do
 	package.cpath = "build/?.so"
-	local filename = arg[0]
-	-- leader
-	local threads = {
-		fw.thread_create(filename, "worker", 1),
-		fw.thread_create(filename, "worker", 2),
-		fw.thread_create(filename, "worker", 3),
-	}
-	fw.thread_join(threads)
-else
-	-- thread
-	local duty, id = ...
-	print("I'm id", id)
+	local fw = require "skynet.framework"
+
+	local function leader()
+		local filename = arg[0]
+		-- leader
+		local threads = {
+			fw.thread_create(filename, "worker", 1),
+			fw.thread_create(filename, "worker", 2),
+			fw.thread_create(filename, "worker", 3),
+		}
+		fw.thread_join(threads)
+	end
+
+	local function worker(id)
+		print("worker", id)
+	end
+
+	if arg then
+		mainfunc = leader
+	else
+		local duty = ...
+		if duty == "worker" then
+			mainfunc = worker
+		end
+	end
 end
 
-
+collectgarbage "collect"	-- collect unused function
+mainfunc(select(2,...))
